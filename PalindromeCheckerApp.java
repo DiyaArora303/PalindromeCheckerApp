67bbe115f442e402e21347efb985bff2794dc981
DiyaArora303 <diyarora303@gmail.com>
2026-03-17 13:35:55 +0530

    Deque-Based Optimized Palindrome Checker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -2,34 +2,28 @@ import java.util.*;
 
 class PalindromeCheckerApp {
     public static void main(String[] args) {
-        
-        String str = "madam";   // Hardcoded string
-        
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-        
-        // Insert into Queue and Stack
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            queue.add(ch);   // Enqueue (FIFO)
-            stack.push(ch);  // Push (LIFO)
-        }
-        
-        boolean isPalindrome = true;
-        
-        // Compare dequeue (queue) and pop (stack)
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        
-        // Display result
-        if (isPalindrome) {
-            System.out.println("The string " + str + " is a palindrome.");
-        } else {
-            System.out.println("The string " + str + " is not a palindrome.");
+    
+    String str = "madam";
+    
+    Deque<Character> deque = new ArrayDeque<>();
+    
+    for (int i = 0; i < str.length(); i++) {
+        deque.addLast(str.charAt(i));
+    }
+    
+    boolean isPalindrome = true;
+    
+    while (deque.size() > 1) {
+        if (deque.removeFirst() != deque.removeLast()) {
+            isPalindrome = false;
+            break;
         }
     }
+    
+    if (isPalindrome) {
+        System.out.println("The string " + str + " is a palindrome.");
+    } else {
+        System.out.println("The string " + str + " is not a palindrome.");
+    }
+}
 }
