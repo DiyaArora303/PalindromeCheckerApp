4123d0bad34d253acfcac385365a3a80e7c77d2f
DiyaArora303 <diyarora303@gmail.com>
2026-03-17 13:56:35 +0530

    Object-Oriented Palindrome Service

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -2,30 +2,37 @@ import java.util.*;
 
 class PalindromeCheckerApp {
     
-    // Recursive function
-    public static void main(String[] args) {
-    
-    String str = "Madam In Eden";
-    
-    str = str.replaceAll("\\s+", "").toLowerCase();
-    
-    int start = 0;
-    int end = str.length() - 1;
-    boolean isPalindrome = true;
-    
-    while (start < end) {
-        if (str.charAt(start) != str.charAt(end)) {
-            isPalindrome = false;
-            break;
+        // Encapsulated method
+    public boolean checkPalindrome(String str) {
+        
+        int start = 0;
+        int end = str.length() - 1;
+        
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        start++;
-        end--;
-    }
-    
-    if (isPalindrome) {
-        System.out.println("The given string is a palindrome (ignoring spaces and case).");
-    } else {
-        System.out.println("The given string is not a palindrome.");
+        
+        return true;
     }
 }
+
+// Main class
+class MainApp {
+    public static void main(String[] args) {
+        
+        
+        String str = "madam";
+        
+        PalindromeCheckerApp checker = new PalindromeCheckerApp();
+        
+        if (checker.checkPalindrome(str)) {
+            System.out.println("The string " + str + " is a palindrome.");
+        } else {
+            System.out.println("The string " + str + " is not a palindrome.");
+        }
+    }
 }
