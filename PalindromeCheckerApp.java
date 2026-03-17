058f19b014eaecdcb382203654af346ba124dab9
DiyaArora303 <diyarora303@gmail.com>
2026-03-17 13:32:17 +0530

    Queue + Stack Based Palindrome Check

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,28 +1,35 @@
-import java.util.Stack;
+import java.util.*;
 
-public class PalindromeCheckerApp {
+class PalindromeCheckerApp {
     public static void main(String[] args) {
-    
-    String str = "madam";
-    Stack<Character> stack = new Stack<>();
-    
-    for (int i = 0; i < str.length(); i++) {
-        stack.push(str.charAt(i));
-    }
-    
-    boolean isPalindrome = true;
-    
-    for (int i = 0; i < str.length(); i++) {
-        if (str.charAt(i) != stack.pop()) {
-            isPalindrome = false;
-            break;
+        
+        String str = "madam";   // Hardcoded string
+        
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        
+        // Insert into Queue and Stack
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            queue.add(ch);   // Enqueue (FIFO)
+            stack.push(ch);  // Push (LIFO)
+        }
+        
+        boolean isPalindrome = true;
+        
+        // Compare dequeue (queue) and pop (stack)
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string " + str + " is a palindrome.");
+        } else {
+            System.out.println("The string " + str + " is not a palindrome.");
         }
     }
-    
-    if (isPalindrome) {
-        System.out.println("The string " + str + " is a palindrome.");
-    } else {
-        System.out.println("The string " + str + " is not a palindrome.");
-    }
-}
 }
